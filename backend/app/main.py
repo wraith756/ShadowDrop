84f134f78f953c14641006a2965470b0b8cfd502
Anurag sharma <anuragsharma73240@gmail.com>
2025-11-29 22:13:16 +0530

    Add new allowed origin for CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173","https://shadow-drop.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
